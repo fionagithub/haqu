69fb05199b0b2658210ad9153f433dd573cdcee0
chaihong <chaihong@jkinfo.com.cn>
2017-06-08 16:46:25 +0800

    target2

--- app/js/menu.directive.js
+++ app/js/menu.directive.js
@@ -1,87 +1,102 @@
  angular.module('common.directives', [])
- .factory('togglefunc',function(){
-     return {
-         getdata:{},
-         getNav:function(o){
-         console.log('---');
-         this.getdata=o;
-         }
-     }
- })
-     /* module map tools  */
-     .directive('menuToggle', function() {
-         return {
-             scope: {
-                 section: '=',
-                 delete: '&',
-                 action: '&',
-                 toggledata: '='
-             },
-             templateUrl: '../view/patch/menu-toggle.tmpl.html',
-             link: function(scope, element) {
-                 var _scope = element.parent().scope();
-                 scope.isOpen = function(obj) {
-                     return _scope.isOpen(obj || scope.section);
-                 };
-                scope.checkMap = function(o) {
-                    _scope.selectedRow(o, function(data) {
-                        if (data.length > 0) {
-                            scope.section.isDel = false;
-                            console.log('存在子数据...');
-                        } else {
-                            scope.section.isDel = true;
-                        }
-                    });
-                };
-                 scope.toggle = function(o) {
-                     scope.toggledata.name = scope.section.name;
-                    if (scope.toggledata.isContent) {
-                        scope.checkMap(scope.section);
-                    }
-                     _scope.toggleOpen(scope.section);
-                 };
-                 scope.isToggled = function() {
-                     if (scope.toggledata.name) {
-                        scope.section.isDel =scope.toggledata.name == scope.section.name;
-                     }
-                 };
-                 scope.setSelectPage = function(i, page) {
-                     _scope.setSelectPage(i, page);
-                 };
+   .factory('commonOpera', function (map, deviceInfo, DeviceField, delDialogService, $rootScope) {
+     var selectedRow = function (obj, callback) {
+       var func,
+         query = {};
+       if (obj.hasOwnProperty('pages') ) {
+         func = map;
+         query[DeviceField.MAP_NO] = obj.id;
+       } else {
+         query[DeviceField.MAP_ID] = obj.id;
+         func = deviceInfo;
+       }
+       $rootScope.query = query;
+       func.filter(null, null, callback)
+       $rootScope.query = query = {};
+     };
+     var deleteData = function (obj) {
+       delDialogService(function () {
+         console.log('delete...');
+         map.deleteOne(obj).then(function (data) {})
+       })
+     };
 
-                 var parentNode = element[0].parentNode.parentNode.parentNode;
-                 if (parentNode.classList.contains('parent-list-item')) {
-                     var heading = parentNode.querySelector('h2');
-                     element[0].firstChild.setAttribute('aria-describedby', heading.id);
-                 }
-             }
+     return {
+       delete: deleteData,
+       selectedRow: selectedRow
+     }
+   })
+   /* module map tools  */
+   .directive('menuToggle', function (commonOpera) {
+     return {
+       scope: {
+         section: '=',
+         delete: '&',
+         action: '&',
+         toggledata: '='
+       },
+       templateUrl: '../view/patch/menu-toggle.tmpl.html',
+       link: function (scope, element) {
+         var _scope = element.parent().scope();
+         scope.isOpen = function (obj) {
+           return _scope.isOpen(obj || scope.section);
          };
-     })
-     .directive('menuLink', function(togglefunc) {
-         return {
-             scope: {
-                 delete: '&',
-                 section: '=',
-                 action: '&',
-                  linkdata: '='
-             },
-             templateUrl: '../view/patch/menu-link.tmpl.html',
-             link: function(scope, element) {
-                 var _scope = element.parent().scope();
-                 scope.mouseActived = true;
-                 scope.isLinked = function() {
-                    if (scope.linkdata.name) {
-                        return scope.linkdata.name == scope.section.name;
-                    }
-                 };
-          
-                scope.focusSection = function() {
-                    scope.linkdata.name = scope.section.name;
-                    _scope.setSelectPage(scope.section);
-                    if (scope.linkdata.isContent) {
-                        _scope.checkMap( scope.section);
-                    }
-                };
-             }
+         scope.flag = {}
+
+         scope.toggle = function (o) {
+           scope.toggledata.name = scope.section.name;
+           if (scope.toggledata.isContent) {
+             commonOpera.selectedRow(scope.section, function (data) {
+               scope.flag.isDel = data.length > 0 ? false : true;
+             });
+           }
+           _scope.toggleOpen(scope.section);
          };
-     })
+         scope.isToggled = function () {
+           if (scope.toggledata.name) {
+             scope.section.isDel = scope.toggledata.name == scope.section.name;
+           }
+         };
+         scope.setSelectPage = function (i, page) {
+           _scope.setSelectPage(i, page);
+         };
+
+         var parentNode = element[0].parentNode.parentNode.parentNode;
+         if (parentNode.classList.contains('parent-list-item')) {
+           var heading = parentNode.querySelector('h2');
+           element[0].firstChild.setAttribute('aria-describedby', heading.id);
+         }
+       }
+     };
+   })
+   .directive('menuLink', function (commonOpera) {
+     return {
+       scope: {
+         delete: '&',
+         section: '=',
+         action: '&',
+         linkdata: '='
+       },
+       templateUrl: '../view/patch/menu-link.tmpl.html',
+       link: function (scope, element) {
+         var _scope = element.parent().scope();
+         scope.flag = {};
+         scope.mouseActived = true;
+         scope.isLinked = function () {
+           if (scope.linkdata.name) {
+             return scope.linkdata.name == scope.section.name;
+           }
+         };
+
+         scope.focusSection = function () {
+           scope.linkdata.name = scope.section.name;
+           _scope.setSelectPage(scope.section);
+           if (scope.linkdata.isContent) {
+             commonOpera.selectedRow(scope.section, function (data) {
+               scope.flag.isDel = data.length > 0 ? false : true;
+             });
+           }
+         };
+       }
+     };
+   })
